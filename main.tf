e5a32ed6ab8d582afd1979b8689c1d5f9f274a99
jspriya <138166363+SpriyaJT@users.noreply.github.com>
2024-04-29 21:32:57 -0500

    change

--- main.tf
+++ main.tf
@@ -51,27 +51,14 @@ resource "aws_instance" "blog" {
 
 
 module "alb" {
-  {
   source  = "terraform-aws-modules/alb/aws"
   version = "9.9.0"
-}
 
-  name            = "blog-alb"
-
-  vpc_id          = "module.blog_vpc.vpc_id"
-  subnets         = "module.blog_vpc.public_subnets"
-  Security_groups = "module.blog_sg.security_group_id"
-
-
-  http_tcp_listeners = [
-  {
-    listener = {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  }
-]
+  name                 = "blog-alb"
+  
+  vpc_id               = module.blog_vpc.vpc_id
+  subnets              = module.blog_vpc.public_subnets
+  security_groups      = [module.blog_sg.security_group_id]
 
   target_groups = [
     {
@@ -79,15 +66,24 @@ module "alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = "aws_instance.blog.id"
-          port      = 80
+      targets          = {
+        my_target      = {
+          target_id    = aws_instance.blog.id
+          port         = 80
         }
       }
     }
- ]
-}
+  ]
+
+  http_tcp_listeners = [
+    {
+      listener = {
+        port               = 80
+        protocol           = "HTTP"
+        target_group_index = 0
+      }
+    }
+  ]
 
   tags = {
     Environment = "Dev"
